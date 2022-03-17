11e8b026e0e87f312d2a139f2422e5c89278e787
Danerinken <shotstrike90@gmail.com>
2022-03-17 23:30:00 +0200

    Удаление лишних комментариев.

--- Event_for_Dex/Program.cs
+++ Event_for_Dex/Program.cs
@@ -6,20 +6,6 @@ namespace Event_for_Dex
     {
         static void Main(string[] args)
         {
-            #region testingEvents no matter
-            //Console.WriteLine("Enter the message:");
-            //string mes = Console.ReadLine();
-
-            //MailManager DNKMailManager = new MailManager();
-            //Printer HP_LJ1012 = new Printer("Hewlett-Packard LaserJet 1012", DNKMailManager);
-            //TV LG = new TV("LG 202029", DNKMailManager);
-            //Sms sms = new Sms(DNKMailManager);
-            //DNKMailManager.SimultateSendingMail(mes);
-            //HP_LJ1012.Dispose();
-            //LG.Dispose();
-            //sms.Dispose();
-
-            #endregion
 
             Person Valeriy = new Person("Валерий", 77711567, "Прага");
             AccountingSystem GoogleAccountSystem = new AccountingSystem();
